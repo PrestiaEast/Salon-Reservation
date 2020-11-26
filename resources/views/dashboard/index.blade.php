6e192badf61f03a92379417b0646040ab3179a4b
Yvonne8867 <Yvonne8867@users.noreply.github.com>
2020-11-27 00:46:15 +0800

    新建 index.blade.php

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,19 @@
+
+    <!DOCTYPE html>  
+<html lang="en">  
+<head>  
+ <meta charset="UTF-8">  
+ <title>Login</title>  
+ 
+</head>  
+<body>  
+ <p id="login">  
+ <h1>Login</h1>  
+ <form method="post">  
+ <input type="text" required="required" placeholder="email" name="u"></input>  
+ <input type="password" required="required" placeholder="password" name="p"></input>  
+ <button class="but" type="submit">login</button>  
+ </form>  
+ </p>  
+</body>  
+</html> 
